a076ae82ed97a907ce3a601d5d914fae6af8b7ce
Allen Chang <achang35@RKC-STE-LD-0001.bu.edu>
2018-11-07 21:23:28 -0500

    Add comments to main script

--- ask_questions.py
+++ ask_questions.py
@@ -1,3 +1,25 @@
+# Copyright 2018 Allen Chang
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ==============================================================================
+
+"""Presents data science interview questions"""
+
 import pandas as pd
 import os
 import random
@@ -10,10 +32,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", help="directory where script is stored")
+    parser.add_argument("--num_questions", help="number of questions per category")
     args = parser.parse_args()
 
     if args.dir:
         path = args.dir
+        num_questions = args.num_questions
 
     filelist = []
     for file in os.listdir(str(path)):
@@ -28,7 +52,7 @@ if __name__ == "__main__":
         file = pd.read_csv(i, header=None, usecols=[0], sep=';')
         file_list = file[0].tolist()
 
-        question_indices = random.sample(range(0, len(file_list)), 5)
+        question_indices = random.sample(range(0, len(file_list)), num_questions)
         question_list = [file_list[i] for i in question_indices]
         questions.append(question_list)
 
